aa09421aba9dd704cc7c31d0f5312708e3e6a5ef
gasb150 <gasb_150@hotmail.com>
2020-12-15 17:47:15 -0500

    add combo pack options

--- lib/catalogue.rb
+++ lib/catalogue.rb
@@ -1,14 +1,46 @@
-#Bottles  Corozo
-          Mango
-          Lulo
-          Guayaba
-          
-#Combos   Vino + Retrato
-          Vino + Copas
-          VIno + txytgcu
-          Vino + sjasdj
 
-#Mayoritario Corozo
-             Mango
-             Lulo
-             Guayaba
+class Wine
+  attr_accessor :pasas
+
+  def single_bottles
+    bottles = {Corozo: p1, Mango: 2000, Lulo: 2000, Guayaba: 2000}
+  end
+  
+  def combo_packs
+    combos  = {wine_atssss: 4000, wine_cups:3000, wine_dinner: 2323, wine_teddy: 1231}
+  end
+  
+  def boxes
+    p @p1
+    mayoritario = {Corozo: @p1*(1-0.1)*12, Mango: 2131, Lulo: 145, Guayaba: 234}
+  end
+
+  def show_combo()
+    @options = self.combo_packs
+    pre_code = @options.keys.to_s.gsub(":wine_", "").gsub("[","").gsub("]","").gsub(" ","").split(",")
+    code = pre_code.map { |x| "#{x}"  }
+    n = 0
+    y = []
+    m = []
+    while n < @options.keys.length do
+    y << [@options.keys[n], "will cost:", @options.values[n], "type this code to select ->", " /#{code[n]}"]
+    m << y[n].join(",").gsub(",", " ")
+    n+=1
+    end
+    # x = Array.new
+    # self.combo_packs.each {|y| x << y}
+    # x
+    m = m.to_s.gsub("," , "\n").gsub('"',"").gsub("[","").gsub("]","").gsub("_"," and ")
+    return m.to_s
+  #to_s.gsub("_", " & ").gsub(":","").gsub("=>", " will cost ").gsub("{","").gsub("}","")
+  
+  end
+end
+ 
+# p n.keys[1]
+# p  n.values
+
+#  person = Wine.new
+#  person.values = 2000
+#  p person.values # => "Dennis"
+
--- lib/hummingbird.rb
+++ lib/hummingbird.rb
@@ -1,13 +1,52 @@
 require 'telegram/bot'
+require_relative 'catalogue'
+
 class BirdBot
   def initialize
     @token = '1406170037:AAEDdK8mS6tYIe3m8jkSUaJenV5rXMeSA2I'
+    @option = Wine.new
     Telegram::Bot::Client.run(@token) do |bird|
-      bird.listen do |text|
-        if text == '/start'
-          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} , welcome to motivation chat bot created by peter robert, the chat bot is to keep you motivated and entertained. Use  /start to start the bot,  /stop to end the bot, /motivate to get a diffrent motivational quo")
-          end
+      bird.listen do |info|
+        case info.text
+        when '/start'
+          bird.api.send_message(chat_id: info.chat.id, text: "Hello, #{info.from.first_name} #{info.from.last_name} this bot will help you to know about our products")
+        when '/combo'
+          bird.api.send_message(chat_id: info.chat.id, text: "You chose combo catalogue, there exist this options, you can select one option using code in the right \n #{@option.show_combo}, if you want to go back to previws menu press '/back' ")
+          bird.listen do |type|
+             case type.text
+             when '/portrait'
+              bird.api.send_message(chat_id: type.chat.id, text: "you choose a portrait and wine")
+            when '/cups'
+              bird.api.send_message(chat_id: type.chat.id, text: "you choose wine and 2 cups")
+            when '/back'
+              break
+             end
+
+            end
+      
+        when '/stop'
+          bird.api.send_message(chat_id: info.chat.id, text: "Good bye, #{info.from.first_name} #{info.from.last_name}")
         end
       end
     end
   end
+end
+
+         #     ---- botones -----
+      
+      #  when Telegram::Bot::Types::CallbackQuery
+      #   # Here you can handle your callbacks from inline buttons
+      #   if info.data == 'touch'
+      #     bird.api.send_message(chat_id: info.from.id, text: "Don't touch me!")
+      #   end
+      #  when Telegram::Bot::Types::Message
+      #   kb = [
+      #     Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Go to Google', url: 'https://google.com'),
+      #     Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Touch me', callback_data: 'touch'),
+      #     Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Switch to inline', switch_inline_query: 'some as')
+      #   ]
+      #   markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+      #   bird.api.send_message(chat_id: info.chat.id, text: 'Make a choice', reply_markup: markup)
+      # end
+         
+  
