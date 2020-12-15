7f9c00291390b4ae6f389805b8d9daa810082d6d
gasb150 <gasb_150@hotmail.com>
2020-12-15 12:12:35 -0500

    add token API

---
+++ lib/catalogue.rb
@@ -0,0 +1,14 @@
+#Bottles  Corozo
+          Mango
+          Lulo
+          Guayaba
+          
+#Combos   Vino + Retrato
+          Vino + Copas
+          VIno + txytgcu
+          Vino + sjasdj
+
+#Mayoritario Corozo
+             Mango
+             Lulo
+             Guayaba
--- lib/hummingbird.rb
+++ lib/hummingbird.rb
@@ -1,4 +1,13 @@
 require 'telegram/bot'
 class BirdBot
-
-end
+  def initialize
+    @token = '1406170037:AAEDdK8mS6tYIe3m8jkSUaJenV5rXMeSA2I'
+    Telegram::Bot::Client.run(@token) do |bird|
+      bird.listen do |text|
+        if text == '/start'
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} , welcome to motivation chat bot created by peter robert, the chat bot is to keep you motivated and entertained. Use  /start to start the bot,  /stop to end the bot, /motivate to get a diffrent motivational quo")
+          end
+        end
+      end
+    end
+  end
